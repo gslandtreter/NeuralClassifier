c8092a478e9db69edc74c5906e8af7dee13a287b
Gustavo S Landtreter <gustavo_landtreter@sicredi.com.br>
2017-10-15 02:18:37 -0200

    Added plotter

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+#include "matplotlib/matplotlibcpp.h"
+namespace plt = matplotlibcpp;
+
+double func(double x) {
+    return sin(x);
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::vector<double> dots;
+    std::vector<double> axis;
+    for(double i = 0; i < 2 * Py_MATH_PI; i+= 0.1) {
+        axis.push_back(i);
+        dots.push_back(func(i));
+    }
+    plt::plot(axis, dots);
+    plt::show();
     return 0;
 }
